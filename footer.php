3bda846f2876ed5f0652fe6e691e3d319e77fd78
IhorPetrunchak <ihorpetrunchak@gmail.com>
2024-01-16 20:31:22 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+              <span><a href="">Виконав студент групи ПМк-42 Петрунчак Ігор</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
